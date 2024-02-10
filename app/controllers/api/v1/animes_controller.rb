2d7e998937b6bac82406f2f5625c27d1df6d831b
AbderrahmanKro <elhairechabderrahman@gmail.com>
2024-02-10 14:35:42 +0100

    Add policy to animes

--- app/controllers/api/v1/animes_controller.rb
+++ app/controllers/api/v1/animes_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::AnimesController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :set_anime, only: [:show]
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  # skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @animes = Anime.all
+    @animes = policy_scope(Anime)
   end
 
   def show
+    authorize @anime
   end
 
   private
